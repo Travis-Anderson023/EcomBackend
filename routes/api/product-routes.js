b158613da88f7421376f535c3ce316bf20e5e14a
Spotexx <tsanderson.023@gmail.com>
2022-02-24 17:00:03 -0500

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const dbProductData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: 'product_tags',
+        },
+      ],
+    });
+    res.json(dbProductData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const dbProductData = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: 'product_tags',
+        },
+      ],
+    });
+    if (!dbProductData) {
+      return res.status(400).json({ message: 'Invalid product id' });
+    }
+    res.json(dbProductData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
