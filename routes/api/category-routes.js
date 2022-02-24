c6dd69bb0da3c6bc5f2e1424a9ef032f897cac3d
Spotexx <tsanderson.023@gmail.com>
2022-02-24 16:35:32 -0500

    category routes updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,53 +3,90 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-
-  Category.findAll({
-    include: [
-      {
-        model: Product
-      }
-    ]
-  }).then(dbCategory => {
-    res.json(dbCategory)
-  }).catch(err => {
-    console.log(err)
-    res.status(501).json(err)
-  })
+router.get('/', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    })
+    await res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  Category.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Product
-      }
-    ]
-  }).then(dbCategoryData => {
+router.get('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findOne({
+
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Product
+        }
+      ]
+    });
     if (!dbCategoryData) {
       return res.status(400).json({ message: "invalid Category id" })
     }
     res.json(dbCategoryData)
-  }).catch(err => {
+  } catch (err) {
     console.log(err)
     res.status(500).json(err)
-  })
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+router.post('/', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.json(dbCategoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!dbCategoryData) {
+      return res.status(400).json({ message: "invalid Category id" })
+    }
+    res.json(dbCategoryData)
+  } catch {
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!dbCategoryData) {
+      return res.status(400).json({ message: "invalid Category id" })
+    }
+    res.json(dbCategoryData)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
