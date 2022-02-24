7f21d8b8534d65209d895bd63818be7e4ee24f87
Spotexx <tsanderson.023@gmail.com>
2022-02-24 17:25:42 -0500

    all routes up"

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,90 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const dbTagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+          as: 'product_tags',
+        },
+      ],
+    });
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const dbTagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+          as: 'product_tags',
+        },
+      ],
+    });
+    if (!dbTagData) {
+      return res.status(400).json({ message: 'Invalid tag id' });
+    }
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const dbTagData = await Tag.create(req.body);
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const dbTagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!dbTagData) {
+      return res.status(400).json({ message: 'Invalid tag id' });
+    }
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const dbTagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!dbTagData) {
+      return res.status(400).json({ message: 'Invalid tag id' });
+    }
+    res.json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
