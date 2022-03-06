696bf1d7804b3fd1295ab4031f3e6d6823f380ff
Spotexx <tsanderson.023@gmail.com>
2022-03-05 19:20:10 -0500

    updated env example and product tag model to fix glitch

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,36 +7,23 @@ class ProductTag extends Model { }
 ProductTag.init(
   {
     // define columns
-    // define columns
     id: {
       type: DataTypes.INTEGER,
-      autoIncrement: true,
       allowNull: false,
+      autoIncrement: true,
       primaryKey: true
     },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
         key: 'id'
       }
     }
